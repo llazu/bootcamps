0fc2eee3f021732bb62dd681041457f2b9a92453
luis <llazu88@gmail.com>
2026-01-07 08:29:49 -0500

    python basic refresher

---
+++ realpython/strings/what-is-a-string.py
@@ -0,0 +1,28 @@
+# Exercise: What is a String?
+
+# Print four individual strings that fulfill certain conditions:
+
+# 1. Double quotation marks inside the string
+
+print('I am what is called a "polymath"')
+print("I am what is called a \"polymath\"")
+
+# 2. An apostrophe inside the string
+
+print('I\'m what is called a polymath')
+print("I'm what is called a polymath") 
+
+# 3. Multiple lines with whitespace preserved
+print("I enjoy many subjects \n such as self-help, psychology, and philosophy.")
+
+# Course solution used triple strings
+print("""I enjoy many subjects 
+such as self-help, psychology, and philosophy.""")
+
+# 4. Coded on multiple lines but get printed on a single line
+print(f"What you "
+      f"doing?")
+
+#Course solution used the \ character
+print("What are you \
+      doing")
