e2287f0f9a9387fa090b7998f10d0ab127ac4ff9
luis <llazu88@gmail.com>
2026-01-10 20:36:18 -0500

    string exercises

---
+++ realpython/strings/string-concatenation.py
@@ -0,0 +1,12 @@
+# Exercise: Concatenation
+
+# Create two strings, concatenate them, and print the resulting string.
+# Can you add a space between them using concatenation?
+
+welcome = "Hi, how you doing?"
+
+message = "My name is Luis."
+
+print(welcome + message)
+
+print(welcome + " " + message)
---
+++ realpython/strings/string-slicing.py
@@ -0,0 +1,13 @@
+# Exercise: Slicing
+
+# Print the string "zing" by using the slice notation
+# to specify the correct range of characters in the
+# the string "bazinga"
+
+orig_string = "bazinga"
+
+print(orig_string[2:6])
+
+# Bonus: Think of more ways to pring zing with slice notation
+
+print(orig_string[-5:-1])
