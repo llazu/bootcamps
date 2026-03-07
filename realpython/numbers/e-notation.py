0aae52d942da6b903f46c8b0e433498f28cd2b9f
luis <llazu88@gmail.com>
2026-03-07 02:44:22 -0500

    numbers exercises

---
+++ realpython/numbers/e-notation.py
@@ -0,0 +1,8 @@
+''' 
+Write a program that assigns the floating-point literal 175000 the 
+variable num using E notation and then prints `num` in the interactive window.
+'''
+
+num = 175e3
+
+print(num)
---
+++ realpython/numbers/max-float.py
@@ -0,0 +1,15 @@
+''' Exercise 3 - Find the Maximum Number
+In IDLE’s interactive window, try to find the smallest exponent N
+for which 2e<N>, where <N> is replaced with your number, returns
+inf.
+'''
+
+n = 200
+
+max = float(f"2e{n}")
+
+while max != float("inf"):
+    n += 1
+    print(f'the current n: {n}')
+    max = float(f"2e{n}")
+    print(f'the current max: {max}')
