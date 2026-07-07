98305954ab41dde2a56b1a1009bc37a624bd8733
luis <llazu88@gmail.com>
2026-07-07 07:53:32 -0400

    new updates

--- realpython/basic/reading-and-writing-files/opening-and-closing-files.py
+++ realpython/basic/reading-and-writing-files/opening-and-closing-files.py
@@ -5,9 +5,17 @@ open("customers-100.csv")
 file = open("customers-100.csv")
 print(file) # print file wrapper
 
+"""
+Failing to close can lead to memory leaksm, Lock other promgrams 
+from accessping the file until python is exited, or it could corrupt
+your data by not flasing the internal buffer on time. 
+"""
+
 file.close() # closed the file
 
 print(file.closed) # checks closed status
+
+
 
 # with behind the scenes
 file = None
@@ -17,11 +25,15 @@ finally:
     if file:
         file.close()
 
-#old skool way
+# old skool way
+# don't need to remember closing the file
+# The drawback of open is that there no abstraction
+# that accounts for OS differences in file path.
 with open("customers-100.csv") as myFile:
     pass
 
-#modern way
+# modern way
+# path open methods doesn't need to provide a string
 from pathlib import Path
 path = Path("customers-100.csv")
 with path.open() as myNewFile:
--- realpython/basic/reading-and-writing-files/text-and-binary-files.py
+++ realpython/basic/reading-and-writing-files/text-and-binary-files.py
@@ -9,7 +9,27 @@ print(chr(99))
 
 # text mode
 file = open("customers-100.csv")
+
 for character in file.read():
     print(character)
 
+# read entire file
 print(file.readline())
+
+# binary mode
+for byte in file.read():
+    print(byte)
+
+ # By default, the file mode for open is text
+
+from pathlib import Path
+
+path = Path("customers-100.csv")
+with path.open(mode="rb") as file: # or rt ot r for text mode
+    print(file.mode)
+    print(file.readable)
+    print(file.writable)
+
+# Potential Problems in Text Mode
+# Character encoding(ASCII, UTF-8, UTF-16)
+# Line Ending  (CR, LF, CRLF)
